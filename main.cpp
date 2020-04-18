2264a24d8288af0f724a0239e23f71ac226b677f
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-18 19:23:08 +0300

    Modify main.cpp to use the relevant classes. gpp  heuristic.cpp agent.cpp MAPPGridState.cpp main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,43 @@
-#include "Astar.cpp"
 #include <unordered_map>
 #include <queue>
 #include<algorithm>
 
+#include"wall.hpp"
+#include "heuristic.hpp"
+#include"agent.hpp"
+#include "MAPPGridState.hpp"
+
+std::vector<Wall> MAPPGridState::walls = {};
+
 int main()
 {
-    // Create walls 
+    /* Create walls */
     MAPPGridState::walls.push_back(Wall(1,1));
     MAPPGridState::walls.push_back(Wall(2,2));
 
     Agent agent_1(0,0, 10,0,"Agent 1");
     Agent agent_2(0,1, 7,5,"Agent 2");
-    Agent agent_3(4,3, 3,4,"Agent 3");
-    Agent agent_4(5,5, 1,1,"Agent 4");
-    Agent agent_5(6,6, 10,10,"Agent 5");
 
-    vector<Agent> agents;
-    //! Reserve the number of agents we will emplace_back
-    agents.reserve(2);
+    std::vector<Agent> agents;
 
     agents.emplace_back(agent_1);
     agents.emplace_back(agent_2);
 
     MAPPGridState grid( agents, 100, 100, 0 );
-    cout<<"↓Original state↓";
+    std::cout<<"↓Original state↓";
     grid.show();
-    vector<MAPPGridState> newStates = grid.successors();
+    std::vector<MAPPGridState> newStates = grid.successors();
 
     /* A* algo */
-    priority_queue<MAPPGridState, vector<MAPPGridState>, CompareStates> Q;
+    std::priority_queue<MAPPGridState, std::vector<MAPPGridState>> Q;
     Q.push( grid );
-    bool going = true;
+    
     MAPPGridState n = grid;
-    unordered_map<MAPPGridState, MAPPGridState, StateHasher> mymap;
-
-    unordered_map<MAPPGridState, unsigned int, StateHasher> minCost;
+    std::unordered_map<MAPPGridState, MAPPGridState> predecessors;
+    std::unordered_map<MAPPGridState, unsigned int> minCost;
 
     minCost.insert({n,0});
+    bool going = true;
     while( !Q.empty() && going )
     {
         n = Q.top();
@@ -59,36 +60,35 @@ int main()
                 minCost.insert( {succ, mcn + 1} );
                 succ.currentCost = mcn + 1;
                 Q.push(succ);
-                mymap.insert({succ,n});
+                predecessors.insert( {succ, n} );
             }
         }
         if( n.getH() == 0 )
         {
             going = false;
-        }
-        if( going == false )
-        {
-            cout<<"Found a result"<<endl;
+            std::cout<<"Found a result"<<std::endl;
         }
     }
     
-    vector<MAPPGridState> results;
+    /* std::vector that holds state trajectory */
+    std::vector<MAPPGridState> results;
     results.reserve(10);
 
     results.emplace_back(n);
-    while( !(mymap.find(n)->second == grid))
+    while( !(predecessors.find(n)->second == grid))
     {
-        n = mymap.find(n)->second;
+        n = predecessors.find(n)->second;
         results.emplace_back(n);
     }
-    n = mymap.find(n)->second;
+    n = predecessors.find(n)->second;
     results.emplace_back(n);
-    std::reverse(results.begin(), results.end());
 
+    std::reverse(results.begin(), results.end());
     for( auto iter = results.begin(); iter < results.end(); ++iter )
     {
         iter->show();
     }
-    cout<<results.size();
+
+    std::cout<<results.size();
     return 0;
 }
